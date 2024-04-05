af12284af88b92f406743e2d8a33e2870cc7534d
Aleksandr Voldieiev <alex.v@pixellu.com>
2024-04-05 12:30:25 +0200

    UPD: lesson 6 dz 13 04.04.2024

--- Tuple and dict/lesson_6_dz_13.py
+++ Tuple and dict/lesson_6_dz_13.py
@@ -1,13 +1,11 @@
 import string
 
-def symbols_between(letters):
-    start, end = letters.split('-')
-    result = ''
-    start_index = string.ascii_letters.index(start)
-    end_index = string.ascii_letters.index(end)
-    for i in range(start_index, end_index + 1):
-        result += string.ascii_letters[i]
-    return result
-
 letters = input("Введіть дві літери через дефіс: ")
-print(symbols_between(letters))
+start, end = letters.split('-')
+result = ''
+start_index = string.ascii_letters.index(start)
+end_index = string.ascii_letters.index(end)
+for i in range(start_index, end_index + 1):
+    result += string.ascii_letters[i]
+
+print(result)
