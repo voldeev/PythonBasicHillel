13dd757a95a8c927deb1a6cd676b52bc104d2caf
Aleksandr Voldieiev <alex.v@pixellu.com>
2024-04-05 12:36:44 +0200

    UPD: lesson 6 dz 15 04.04.2024

--- Tuple and dict/lesson_6_dz_15.py
+++ Tuple and dict/lesson_6_dz_15.py
@@ -4,5 +4,15 @@ days, remaining_seconds = divmod(total_seconds, 24 * 60 * 60)
 hours, remaining_seconds = divmod(remaining_seconds, 60 * 60)
 minutes, seconds = divmod(remaining_seconds, 60)
 
-output = f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+# Вибираємо правильне слово для "день" на основі кількості днів
+if days == 1:
+    days_word = "день"
+elif days % 10 == 1 and days % 100 != 11:
+    days_word = "день"
+elif 2 <= days % 10 <= 4 and (days % 100 < 10 or days % 100 >= 20):
+    days_word = "дні"
+else:
+    days_word = "днів"
+
+output = f"{days} {days_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 print(output)
