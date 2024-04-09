033102d0804b376384b2c50ff35e1793e83298f9
Aleksandr Voldieiev <alex.v@pixellu.com>
2024-04-09 10:37:09 +0200

    UPD: lesson 10 dz 25, 26, 27 - 09.04.2024

---
+++ Decorators/lesson_10_dz_25.py
@@ -0,0 +1,20 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    result = begin
+    for _ in range(end):
+        yield result
+        result = func(result)
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
